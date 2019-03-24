07c1128d35480ede15d6c7cd971d05b1f8a18512
ahribori <ahribori@gmail.com>
2019-03-25 00:34:33 +0900

    DOM Draggable하게 만들기

--- src/components/ResizeDragSnap/Element.tsx
+++ src/components/ResizeDragSnap/Element.tsx
@@ -11,6 +11,10 @@ class Element extends Component<IProps, IState> {
   ON_LEFT_EDGE: boolean = false;
   ON_RIGHT_EDGE: boolean = false;
   ON_BOTTOM_EDGE: boolean = false;
+  ON_EDGE: boolean = false;
+  ON_MOVE: boolean = false;
+  offsetX: number = 0;
+  offsetY: number = 0;
 
   handleMouseMove: React.MouseEventHandler<HTMLElement> = e => {
     const boundingClientRect = (e.target as HTMLElement).getBoundingClientRect();
@@ -22,7 +26,50 @@ class Element extends Component<IProps, IState> {
     this.ON_LEFT_EDGE = x < this.MARGINS;
     this.ON_RIGHT_EDGE = x >= width - this.MARGINS;
     this.ON_BOTTOM_EDGE = y >= height - this.MARGINS;
+    this.ON_EDGE =
+      this.ON_TOP_EDGE ||
+      this.ON_BOTTOM_EDGE ||
+      this.ON_LEFT_EDGE ||
+      this.ON_RIGHT_EDGE;
     this.setCursorStyle(e.target);
+
+    if (this.ON_MOVE) {
+      const nodeElement = ReactDOM.findDOMNode(
+        e.target as HTMLElement,
+      ) as HTMLElement;
+      nodeElement.style.left = `${clientX - this.offsetX}px`;
+      nodeElement.style.top = `${clientY - this.offsetY}px`;
+    }
+  };
+
+  handleMouseDown: React.MouseEventHandler<HTMLElement> = e => {
+    if (this.ON_EDGE) {
+    } else {
+      const boundingClientRect = (e.target as HTMLElement).getBoundingClientRect();
+      const { left, top } = boundingClientRect;
+      const { clientX, clientY } = e;
+      this.ON_MOVE = true;
+      this.offsetX = clientX - left;
+      this.offsetY = clientY - top;
+    }
+  };
+
+  handleMouseUp: React.MouseEventHandler<HTMLElement> = e => {
+    if (this.ON_EDGE) {
+    } else {
+      if (this.ON_MOVE) {
+        this.ON_MOVE = false;
+      }
+    }
+  };
+
+  handleMouseLeave: React.MouseEventHandler<HTMLElement> = e => {
+    if (this.ON_EDGE) {
+    } else {
+      if (this.ON_MOVE) {
+        this.ON_MOVE = false;
+      }
+    }
   };
 
   setCursorStyle = (node: EventTarget) => {
@@ -34,7 +81,7 @@ class Element extends Component<IProps, IState> {
     } else if (this.ON_TOP_EDGE || this.ON_BOTTOM_EDGE) {
       nodeElement.style.cursor = 'ns-resize';
     } else {
-      nodeElement.style.cursor = 'default';
+      nodeElement.style.cursor = 'move';
     }
   };
 
@@ -42,8 +89,12 @@ class Element extends Component<IProps, IState> {
     return (
       <div
         onMouseMove={this.handleMouseMove}
+        onMouseDown={this.handleMouseDown}
+        onMouseUp={this.handleMouseUp}
+        onMouseLeave={this.handleMouseLeave}
         style={{
           display: 'inline-block',
+          position: 'absolute',
           width: 200,
           height: 200,
           margin: 4,
