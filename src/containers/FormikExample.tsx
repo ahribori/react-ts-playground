13093ee16ae6beb75d78c1738c3849187d4646db
Daniel <ahribori@gmail.com>
2021-03-11 00:15:48 +0900

    Formik checkbox examples

--- src/containers/FormikExample.tsx
+++ src/containers/FormikExample.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useFormik } from 'formik';
 
 const FormikExample = () => {
-  return <div>Formik</div>;
+  const formik = useFormik({
+    initialValues: {
+      items: [],
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  console.log(formik.values.items);
+  return (
+    <div>
+      {['apple', 'banana', 'orange'].map((item) => (
+        <div key={item}>
+          <input
+            type="checkbox"
+            name="items"
+            id={item}
+            value={item}
+            onChange={formik.handleChange}
+          />
+          <label htmlFor={item}>{item}</label>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default FormikExample;
